91c651f19e2656526462f97e5466158fe4304e83
Brent Gardner <bgardner@squarelabs.net>
2019-07-02 11:55:34 -0600

    initial commit

---
+++ wasm_softcore/wasm_softcore.runs/System_wasm_softcore_0_0_synth_1/System_wasm_softcore_0_0_stub.v
@@ -0,0 +1,52 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Tue Jul  2 11:11:53 2019
+// Host        : bgmsi running 64-bit Ubuntu 18.04.2 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ System_wasm_softcore_0_0_stub.v
+// Design      : System_wasm_softcore_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z007sclg225-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "wasm_softcore,Vivado 2019.1" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clka, ena, rsta, wea, addra, dina, douta, rsta_busy, 
+  S_AXI_ACLK, S_AXI_ARESETN, S_AXI_AWADDR, S_AXI_AWPROT, S_AXI_AWVALID, S_AXI_AWREADY, 
+  S_AXI_WDATA, S_AXI_WSTRB, S_AXI_WVALID, S_AXI_WREADY, S_AXI_BRESP, S_AXI_BVALID, 
+  S_AXI_BREADY, S_AXI_ARADDR, S_AXI_ARPROT, S_AXI_ARVALID, S_AXI_ARREADY, S_AXI_RDATA, 
+  S_AXI_RRESP, S_AXI_RVALID, S_AXI_RREADY)
+/* synthesis syn_black_box black_box_pad_pin="clka,ena,rsta,wea[3:0],addra[31:0],dina[31:0],douta[31:0],rsta_busy,S_AXI_ACLK,S_AXI_ARESETN,S_AXI_AWADDR[3:0],S_AXI_AWPROT[2:0],S_AXI_AWVALID,S_AXI_AWREADY,S_AXI_WDATA[31:0],S_AXI_WSTRB[3:0],S_AXI_WVALID,S_AXI_WREADY,S_AXI_BRESP[1:0],S_AXI_BVALID,S_AXI_BREADY,S_AXI_ARADDR[3:0],S_AXI_ARPROT[2:0],S_AXI_ARVALID,S_AXI_ARREADY,S_AXI_RDATA[31:0],S_AXI_RRESP[1:0],S_AXI_RVALID,S_AXI_RREADY" */;
+  output clka;
+  output ena;
+  output rsta;
+  output [3:0]wea;
+  output [31:0]addra;
+  output [31:0]dina;
+  input [31:0]douta;
+  input rsta_busy;
+  input S_AXI_ACLK;
+  input S_AXI_ARESETN;
+  input [3:0]S_AXI_AWADDR;
+  input [2:0]S_AXI_AWPROT;
+  input S_AXI_AWVALID;
+  output S_AXI_AWREADY;
+  input [31:0]S_AXI_WDATA;
+  input [3:0]S_AXI_WSTRB;
+  input S_AXI_WVALID;
+  output S_AXI_WREADY;
+  output [1:0]S_AXI_BRESP;
+  output S_AXI_BVALID;
+  input S_AXI_BREADY;
+  input [3:0]S_AXI_ARADDR;
+  input [2:0]S_AXI_ARPROT;
+  input S_AXI_ARVALID;
+  output S_AXI_ARREADY;
+  output [31:0]S_AXI_RDATA;
+  output [1:0]S_AXI_RRESP;
+  output S_AXI_RVALID;
+  input S_AXI_RREADY;
+endmodule
